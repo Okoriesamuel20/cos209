bf7ccb472dd6115bda8875b27386d8c0c1c35e57
Okoriesamuel20 <samueokorie2004@gmail.com>
2024-12-01 08:32:24 +0100

    Add files via upload

---
+++ bigsam.js
@@ -0,0 +1,23 @@
+let person =
+{
+    name: 'okorie samuel ikechukwu',
+    age :43,
+   faculty: 'agricultural technoloy',
+   department: 'animal science',
+   'Matric number': '2021/131882 regular',
+   Email: 'samuelokorie2004@gmail.com',
+  Nationality: 'nigerian',
+  State: 'abia state',
+  LGA: 'BENDE',
+  Grades:  {
+    Mathematics: 'A1',
+    physics: 'B2',
+    'Use of english': 'A1',
+    biology: 'C4',
+    'AGRIC SCIENCE': 'A1',
+    'animal husbandry': 'B2',
+    'Civic education': 'c4'
+  }
+};
+
+console.log(person)
